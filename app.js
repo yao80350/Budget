ea3a945989bd87c7426221df4c8cf419092c5c4d
yao80350 <yao80350@gmail.com>
2019-08-20 11:12:50 +0800

    068 Implementing the Module Pattern

---
+++ app.js
@@ -0,0 +1,11 @@
+var budgetController = (function () {
+
+})();
+
+var UIController = (function () {
+
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+})(budgetController, UIController);
