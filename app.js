bf95cdeeba0b906dee34b67c49903fb5fd025d3e
yao80350 <yao80350@gmail.com>
2019-08-21 09:13:00 +0800

    071 Creating an Initialization Function

--- app.js
+++ app.js
@@ -34,21 +34,29 @@ var controller = (function (budgetCtrl, UICtrl) {
     function ctrlAddItem() {
         // 1. Get the field input date
         var input = UICtrl.getInput();
-        console.log(input);
 
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the Ui
-        console.log('add');
     }
 
-    document.querySelector(domString.inputBtn).addEventListener('click', ctrlAddItem);
+    var setupEventListeners = function () {
+        document.querySelector(domString.inputBtn).addEventListener('click', ctrlAddItem);
 
-    document.addEventListener('keydown', function(event) {
-        // event.which for old browber
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+        document.addEventListener('keydown', function(event) {
+            // event.which for old browber
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            setupEventListeners();
         }
-    });
+    }
 })(budgetController, UIController);
+
+controller.init();
